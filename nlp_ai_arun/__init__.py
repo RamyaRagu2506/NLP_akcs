e284d2a61b6f2de97ff4933abc7a925ecc41a0ea
Srinivas <sm1043@gmail.com>
2023-05-20 20:29:49 +0530

    excel file access changed

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -12,8 +12,12 @@ def main(NBDblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {NBDblob.name} \n"
                  f"Blob Size: {NBDblob.length} bytes")
+    
     try:
         
+        account_name = "https://arunakcs.blob.core.windows.net/"
+        excel_complete_path = account_name + NBDblob.name
+        logging.info(excel_complete_path)
         # # Connect to the Azure Blob storage using the connection string
         # connection_string = "DefaultEndpointsProtocol=https;AccountName=arunakcs;AccountKey=nx8T5960W1vcaeHKOD/4HtiCm0/n58VXhtsNAp7LoyDdZX6IdRPsomJsBoOgB72wPd9AHfwwcoFo+AStndZq2Q==;EndpointSuffix=core.windows.net"
         # blob_service_client = BlobServiceClient.from_connection_string(connection_string)
@@ -24,7 +28,7 @@ def main(NBDblob: func.InputStream):
         # blob_client = container_client.get_blob_client(myblob.name)
 
         # excel_data = blob_client.download_blob().readall()
-        df = pd.read_excel(NBDblob.name)
+        df = pd.read_excel(excel_complete_path)
         logging.df(df.head())
         
     except Exception as e:
