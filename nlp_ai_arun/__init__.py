23751faad73c7a0695f446227b248bb4e9ae71fb
Srinivas <sm1043@gmail.com>
2023-05-20 18:00:33 +0530

    changed code

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -6,23 +6,23 @@ import io
 
 
 
-def main(myblob: func.InputStream):
+def main(NBDblob: func.InputStream, CBDblob: func.InputStream, CLT_RAKblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+                 f"Name: {NBDblob.name}. {CBDblob.name}, {CLT_RAKblob.name}\n"
+                 f"Blob Size: {NBDblob.length} bytes")
     try:
         
-        # Connect to the Azure Blob storage using the connection string
-        connection_string = "DefaultEndpointsProtocol=https;AccountName=arunakcs;AccountKey=nx8T5960W1vcaeHKOD/4HtiCm0/n58VXhtsNAp7LoyDdZX6IdRPsomJsBoOgB72wPd9AHfwwcoFo+AStndZq2Q==;EndpointSuffix=core.windows.net"
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        # # Connect to the Azure Blob storage using the connection string
+        # connection_string = "DefaultEndpointsProtocol=https;AccountName=arunakcs;AccountKey=nx8T5960W1vcaeHKOD/4HtiCm0/n58VXhtsNAp7LoyDdZX6IdRPsomJsBoOgB72wPd9AHfwwcoFo+AStndZq2Q==;EndpointSuffix=core.windows.net"
+        # blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
-        # Get the blob container and blob client
-        container_name = "excelfiles"
-        container_client = blob_service_client.get_container_client(container_name)
-        blob_client = container_client.get_blob_client(myblob.name)
-        
-        excel_data = blob_client.download_blob().readall()
-        df = pd.read_excel(io.BytesIO(excel_data))
+        # # Get the blob container and blob client
+        # container_name = "excelfiles"
+        # container_client = blob_service_client.get_container_client(container_name)
+        # blob_client = container_client.get_blob_client(myblob.name)
+
+        # excel_data = blob_client.download_blob().readall()
+        df = pd.read_excel(NBDblob.name)
         logging.df(df.head())
         
     except Exception as e:
