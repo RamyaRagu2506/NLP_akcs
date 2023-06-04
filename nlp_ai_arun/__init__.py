136f676deed661b51736becf4d84e21354b1b199
Srinivas <sm1043@gmail.com>
2023-06-04 08:19:19 +0530

    Changes on cbd and cltrak

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -103,11 +103,13 @@ def nlp_algo_comparison(file_path, template_path):
         logging.info(f"Done and dusted!")
 
     
-def main(NBDblob: func.InputStream):
+def main(NBDblob: func.InputStream, CBDblob: func.InputStream, CLTRAKBlob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {NBDblob.name} \n"
                  f"Blob Size: {NBDblob.length} bytes \n"
                  f"Blob modified Date: {NBDblob.metadata.get('last_modified')}" )
+    
+    logging.info(f"cltrak: {CLTRAKBlob.name}, cbd: {CBDblob.name}")
     
     try:
         account_name = "https://arunakcs.blob.core.windows.net/"
