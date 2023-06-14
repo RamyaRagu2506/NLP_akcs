3313362fbc90d00a1a03cda50568977894df8cec
Srinivas <sm1043@gmail.com>
2023-06-14 11:08:55 +0530

    added template df

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -15,4 +15,8 @@ def main(myblob: func.InputStream):
     logging.info(f"Blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")  
+    template_file_path = "https://arunakcs.blob.core.windows.net/excelfiles/main_template/test_template.xlsx"
+    df_template = pd.read_excel(template_file_path)
+    logging.info(df_template.columns)
+    
     
