1499f5c62dc12b2c1ebd8cf39ba3081b6ddd50de
Srinivas <sm1043@gmail.com>
2023-06-14 11:49:14 +0530

    setup sql

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -2,13 +2,9 @@ import logging
 import pandas as pd
 import azure.functions as func
 import logging
-import re
-import datetime
-from datetime import datetime as second_datetime
 from azure.storage.blob import BlobServiceClient
-import io
-import requests
-
+import os
+import pyodbc
     
 def main(myblob: func.InputStream):
     
@@ -18,5 +14,39 @@ def main(myblob: func.InputStream):
     template_file_path = "https://arunakcs.blob.core.windows.net/excelfiles/main_template/test_template.xlsx"
     df_template = pd.read_excel(template_file_path)
     logging.info(df_template.columns)
+    
+    
+    server = "akcserver.database.windows.net"
+    database = "dbarunsql"
+    username = "Arun"
+    password = "Asds@2022"
+    table_name = "TransactionDetails"
+
+    try:
+        # Establish the database connection
+        conn_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+        conn = pyodbc.connect(conn_str)
+
+        # Insert blob details into the SQL table
+        cursor = conn.cursor()
+        # cursor.execute(f"INSERT INTO {table_name} (FileName, BlobSize) VALUES (?, ?)", name, blob.length)
+        # conn.commit()
+
+        # Read rows from the SQL table
+        cursor.execute(f"SELECT * FROM {table_name}")
+        rows = cursor.fetchall()
+
+        # Process the retrieved data
+        result = []
+        for row in rows:
+            result.append(list(row))
+
+        logging.info(result)
+
+    except Exception as e:
+        logging.error(e)
+        raise
+    finally:
+        conn.close()
     
     
