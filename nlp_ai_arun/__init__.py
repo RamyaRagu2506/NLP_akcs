e911de470913ef038959994be08deee0fbbe048d
Srinivas <sm1043@gmail.com>
2023-06-20 22:20:07 +0530

    changed code

--- nlp_ai_arun/__init__.py
+++ nlp_ai_arun/__init__.py
@@ -462,11 +462,10 @@ def main(myblob: func.InputStream):
         update_sql_table_for_classified(pdf_based_file_preprocessed_data, SERVER, DATABASE, USERNAME, PASSWORD, TRANSACTIONDETAILSTABLENAME)
         populate_report_template = populate_final_report(report_template, pdf_based_file_preprocessed_data, input_file, SERVER, DATABASE, USERNAME, PASSWORD)
         
-        
+        logging.info(f"{input_file}")
         if 'Emirates-NBD-Classic-Luxury-Main' in input_file:
             file_name_xl='Emirates-NBD-Classic-Luxury-Main'  
             logging.info(f"{file_name_xl}")    
-            
         elif 'Rak-Bank-Classic-Luxury' in input_file:
             file_name_xl='Rak-Bank-Classic-Luxury'
             logging.info(f"{file_name_xl}")
@@ -482,7 +481,7 @@ def main(myblob: func.InputStream):
         elif 'OLT-Emirates-Islamic-Bank' in input_file:
             file_name_xl='OLT-Emirates-Islamic-Bank'
             logging.info(f"{file_name_xl}")
-        elif 'Emirates-NBD-Classic-Passenger' in input_file:
+        elif "Emirates-NBD-Classic-Passenger" in input_file:
             file_name_xl='Emirates-NBD-Classic-Passenger'
             logging.info(f"{file_name_xl}")
         elif 'ENBD-Classic-Riders' in input_file:
